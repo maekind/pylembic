a81c900ccaf7064beedb80cab011b0c8d0ed430e
Marco Espinosa <marco@kidoodle.tv>
2024-12-19 17:19:17 +0100

    Improve formatter

--- src/pylembic/logger.py
+++ src/pylembic/logger.py
@@ -19,16 +19,13 @@ def configure_logger(verbose: bool = False) -> logging.Logger:
         logger.handlers.clear()
 
     if not verbose:
-        logger.setLevel(logging.CRITICAL + 1)  # Effectively disables logging
+        # Effectively disables logging
+        logger.setLevel(logging.CRITICAL + 1)
         return logger
 
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    formatter = CustomFormatter(
-        "%(levelname)s\t %(asctime)s | %(message)s | %(migration)s"
-        "%(dependency)s%(orphans)s%(heads)s%(bases)s%(up_revisions)s%(down_revisions)s",
-        datefmt="%d %b %Y | %H:%M:%S",
-    )
+    formatter = CustomFormatter(datefmt="%d %b %Y | %H:%M:%S")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
